a768d2dae964dab377db10e8f8bc046f221d068a
Your Name <you@example.com>
2026-09-25 20:26:02 +0800

    fix(frontend): 复核任务类型列表隐藏 ID 列
    ID 是后端 uuid，对使用者没有意义，去掉该列（表头/单元格/colgroup 一并对齐，
    空态与加载中行的 colspan 由 8 改为 7），首列改为「算法名称」。

--- frontend/src/pages/ReviewTypeListPage.vue
+++ frontend/src/pages/ReviewTypeListPage.vue
@@ -4,15 +4,15 @@
     <div class="review-board">
       <div class="algorithm-toolbar"><button class="btn primary" @click="openCreate">新建</button></div>
       <table class="prototype-table">
-        <colgroup><col style="width:auto;" /><col style="width:170px;" /><col style="width:210px;" /><col /><col style="width:110px;" /><col style="width:125px;" /><col style="width:150px;" /><col style="width:100px;" /></colgroup>
-        <thead><tr><th>ID</th><th class="left">算法名称</th><th class="left">算法编码</th><th class="left">提示词</th><th class="left">备注</th><th>注入事件字段</th><th>更新时间</th><th>操作</th></tr></thead>
+        <colgroup><col style="width:170px;" /><col style="width:210px;" /><col /><col style="width:110px;" /><col style="width:125px;" /><col style="width:150px;" /><col style="width:100px;" /></colgroup>
+        <thead><tr><th class="left">算法名称</th><th class="left">算法编码</th><th class="left">提示词</th><th class="left">备注</th><th>注入事件字段</th><th>更新时间</th><th>操作</th></tr></thead>
         <tbody>
           <tr v-for="row in rows" :key="row.id">
-            <td :title="row.id" style="white-space:nowrap;">{{ row.id }}</td><td class="left">{{ row.name }}</td><td class="left">{{ row.code }}</td><td class="left ellipsis">{{ row.prompt }}</td><td class="left ellipsis">{{ row.remark }}</td><td><span v-if="row.injectEvent" class="mini-tag">{{ row.injectEvent }}</span><span v-else>-</span></td><td>{{ formatTime(row.updatedAt) }}</td>
+            <td class="left">{{ row.name }}</td><td class="left">{{ row.code }}</td><td class="left ellipsis">{{ row.prompt }}</td><td class="left ellipsis">{{ row.remark }}</td><td><span v-if="row.injectEvent" class="mini-tag">{{ row.injectEvent }}</span><span v-else>-</span></td><td>{{ formatTime(row.updatedAt) }}</td>
             <td><button class="link-blue" @click="openEdit(row)">编辑</button><button class="link-red" @click="remove(row)">删除</button></td>
           </tr>
-          <tr v-if="!loading && !rows.length"><td colspan="8" class="empty-cell">暂无复核类型</td></tr>
-          <tr v-if="loading"><td colspan="8" class="empty-cell">加载中...</td></tr>
+          <tr v-if="!loading && !rows.length"><td colspan="7" class="empty-cell">暂无复核类型</td></tr>
+          <tr v-if="loading"><td colspan="7" class="empty-cell">加载中...</td></tr>
         </tbody>
       </table>
     </div>
